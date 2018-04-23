49a14d6bbc18fd3361f1448cdacff7902d451f08
Anthony Nadaletti <anthony.nadaletti@gmail.com>
2018-04-22 23:42:35 -0300

    Navbar e mainbanner

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 // Função executada quando clica no ícone de barras (responsividade)
 document.getElementById('responsiveBars').addEventListener("click", function() {
     let nav = document.getElementsByClassName('nav-list')[0];
-    if(nav.style.display == '')
+    let logo = document.getElementsByClassName('nav-logo')[0];
+    if(nav.style.display == ''){
         nav.style.display = 'flex';
-    else
+        logo.style.display = 'none';
+    }
+    else{
         nav.style.display = '';
+        logo.style.display = '';
+    }
 });
