c990aab3170e0ca2675b59e8dfa7019a469cbd3a
Anthony Nadaletti <anthony.nadaletti@gmail.com>
2018-04-25 09:46:48 -0300

    Começando local de trabalho

--- js/main.js
+++ js/main.js
@@ -1,13 +1,29 @@
 // Função executada quando clica no ícone de barras (responsividade)
-document.getElementById('responsiveBars').addEventListener("click", function() {
+document.getElementById('responsiveBars').addEventListener("click", function () {
     let nav = document.getElementsByClassName('nav-list')[0];
     let logo = document.getElementsByClassName('nav-logo')[0];
-    if(nav.style.display == ''){
+    if (nav.style.display == '') {
         nav.style.display = 'flex';
         logo.style.display = 'none';
-    }
-    else{
+    } else {
         nav.style.display = '';
         logo.style.display = '';
+    }
+});
+
+document.getElementById('imgT').addEventListener("click", function () {
+    if (this.dataset.zoomed == '0'){
+        this.style.position = 'absolute';
+        this.style.top = '1675px';
+        this.style.left = '14%'
+        this.style.width = '1000px';
+        this.dataset.zoomed = '1';
+    }
+    else {
+        this.style.position = '';
+        this.style.top = '';
+        this.style.left = ''
+        this.style.width = '';
+        this.dataset.zoomed = '0';
     }
 });
